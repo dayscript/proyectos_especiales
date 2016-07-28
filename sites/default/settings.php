ff18f0046c93092596d589495fc12170b9bd75fa
Juan Carlos Orrego <jcorrego@gmail.com>
2016-07-28 15:48:14 -0500

    asd

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -83,11 +83,13 @@
  * webserver.  For most other drivers, you must specify a
  * username, password, host, and database name.
  *
- * Some database engines support transactions.  In order to enable
- * transaction support for a given database, set the 'transactions' key
- * to TRUE.  To disable it, set it to FALSE.  Note that the default value
- * varies by driver.  For MySQL, the default is FALSE since MyISAM tables
- * do not support transactions.
+ * Transaction support is enabled by default for all drivers that support it,
+ * including MySQL. To explicitly disable it, set the 'transactions' key to
+ * FALSE.
+ * Note that some configurations of MySQL, such as the MyISAM engine, don't
+ * support it and will proceed silently even if enabled. If you experience
+ * transaction related crashes with such configuration, set the 'transactions'
+ * key to FALSE.
  *
  * For each database, you may optionally specify multiple "target" databases.
  * A target database allows Drupal to try to send certain queries to a
@@ -199,7 +201,7 @@
  *   $databases['default']['default'] = array(
  *     'driver' => 'pgsql',
  *     'database' => 'databasename',
- *     'username' => 'username',
+ *     'username' => 'username', 
  *     'password' => 'password',
  *     'host' => 'localhost',
  *     'prefix' => '',
@@ -208,23 +210,23 @@
  *     'driver' => 'sqlite',
  *     'database' => '/path/to/databasefilename',
  *   );
- * @endcode 
+ * @endcode
  */
-$databases = array (
-  'default' => 
-  array (
-    'default' => 
-    array (
-      'database' => 'especiales',
-      'username' => 'pantheon',
-      'password' => 'f07ef67d15984cbeac9f2d0227d88e8b',
-      'host'     => 'dbserver.dev.4fe264bc-32c0-46fb-8a83-cbc729bb0191.drush.in',
-      'port'     => '14064',
-      'driver' => 'mysql',
-      'prefix' => '',
-    ),
-  ),
-);
+ $databases = array (
+   'default' =>
+   array (
+     'default' =>
+     array (
+       'database' => 'especiales',
+       'username' => 'forge',
+       'password' => 'cOgjZoIGk9eK3e4LFIfM',
+       'host' => 'localhost',
+       'port' => '',
+       'driver' => 'mysql',
+       'prefix' => '',
+     ),
+   ),
+ );
 
 /**
  * Access control for update.php script.
@@ -256,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'gjgL51J1_4lPqGljz8M2Yz7NYR4qlzaj00vO2BSRiSk';
+$drupal_hash_salt = '';
 
 /**
  * Base URL (optional).
@@ -447,6 +449,18 @@ ini_set('session.cookie_lifetime', 2000000);
 # $conf['js_gzip_compression'] = FALSE;
 
 /**
+ * Block caching:
+ *
+ * Block caching may not be compatible with node access modules depending on
+ * how the original block cache policy is defined by the module that provides
+ * the block. By default, Drupal therefore disables block caching when one or
+ * more modules implement hook_node_grants(). If you consider block caching to
+ * be safe on your site and want to bypass this restriction, uncomment the line
+ * below.
+ */
+# $conf['block_cache_bypass_node_grants'] = TRUE;
+
+/**
  * String overrides:
  *
  * To override specific strings on your site with or without enabling the Locale
@@ -542,20 +556,6 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
 # $conf['proxy_user_agent'] = '';
 # $conf['proxy_exceptions'] = array('127.0.0.1', 'localhost');
 
-
-/*
-$conf['cache_backends'][] = 'sites/all/modules/memcache/memcache.inc';
-$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
-$conf['cache_default_class'] = 'MemCacheDrupal';
-$conf['memcache_servers'] = array(
-        'localhost:11211' => 'cluster'
-);
-
-$conf['memcache_bins'] = array('cache' => 'default');
-$conf['memcache_key_prefix'] = 'proyectosespeciales_';*/
-
-
-
 /**
  * Authorized file system operations:
  *
@@ -579,9 +579,3 @@ $conf['memcache_key_prefix'] = 'proyectosespeciales_';*/
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
-
-/**
- * Add the domain module setup routine.
- */
-include DRUPAL_ROOT . '/sites/all/modules/domain/settings.inc';
-
