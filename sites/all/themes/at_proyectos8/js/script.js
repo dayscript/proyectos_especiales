63977abdec0f8b03771b229004f1d1648c5dc2fd
cdvillamil92 <cvillamil@dayscript.com>
2017-11-24 18:50:06 -0500

    update git

--- sites/all/themes/at_proyectos8/js/script.js
+++
@@ -1,8 +0,0 @@
-
-(function ($, Drupal, window, document, undefined) {
-	$(document).ready(function($) {
-		if ($(window).width() >= 1000) {
-	    $("#header .logo-contexto img").removeAttr("src"); 
-	  }
-	});
-})(jQuery, Drupal, this, this.document);
